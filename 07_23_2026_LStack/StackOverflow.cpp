be0ab2d5e140f7a8a0e88c3eb6df04e68b703076
Ali Azhari <aliazhari@gmail.com>
2026-07-24 16:05:18 -0400

    Adding StackOverflow.cpp

---
+++ 07_23_2026_LStack/StackOverflow.cpp
@@ -0,0 +1,30 @@
+/* **********************************************************
+*  Author : Ali Azhari   
+*  Created On : Thu Jul 23 2026
+*  File : StackOverflow.cpp
+*  Description: The StackOverflow class defines a simple custom 
+*               exception that is thrown when an attempt is made 
+*               to push an element onto a full stack. 
+*               It stores an error message that can be customized 
+*               through its constructor, while providing a default 
+*               message of "StackOverflow". 
+*               The what() member function returns the error message, 
+*               allowing the program to report the reason for the 
+*               exception when it is caught.
+*****************************************************************/
+
+#include <string>
+class StackOverflow {
+    private:
+        std::string msg = "StackOverflow";
+    public:
+        StackOverflow() : StackOverflow(msg) {}
+
+        StackOverflow(std::string msg) {
+            this->msg = msg;
+        }
+        std::string what() {
+            return msg;
+        }
+};
+
