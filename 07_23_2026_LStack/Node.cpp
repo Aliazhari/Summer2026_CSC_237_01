e2ad20bd0d1ad485921ac8d10f99e470591e7495
Ali Azhari <aliazhari@gmail.com>
2026-07-24 16:52:19 -0400

    Adding and complete Node.cpp

---
+++ 07_23_2026_LStack/Node.cpp
@@ -0,0 +1,33 @@
+#ifndef NODE_CPP
+#define NODE_CPP
+/* ***************************************************************
+*  Author : Ali Azhari   
+*  Created On : Thu Jul 23 2026
+*  File : Node.cpp
+*  Description: Node.cpp contains the implementation of 
+*               the Node class used by the linked-list-based stack. 
+*               It defines the node constructor, which initializes 
+*               the node's data and sets the pointer to the next node. 
+*               Each node represents a single element in the stack 
+*               and is dynamically allocated as elements are pushed 
+*               onto the stack.
+* *****************************************************************/
+
+template <class T>
+class Node {
+    private:
+        T data;
+        Node<T> *next;
+
+    public:
+        Node(T data)
+        {
+            this->data = data;
+            next = nullptr;
+        }
+        T getData() const { return data; }
+        Node<T> getNext() const { return next; }
+        void setData(T data) { this->data = data; }
+        void setNext(Node<T> next) { this->next = next; }
+};
+#endif
